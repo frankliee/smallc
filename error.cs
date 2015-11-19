e39158c4ccee1ee3a9f0ebbd1e6d07b3fcd56a6f
yestodaylee <marslee@marsleedeMacBook-Air.local>
2015-11-19 16:01:13 +0800

    add error.cs

---
+++ error.cs
@@ -0,0 +1,22 @@
+var i;
+write('for');
+while(1==1)
+for( i=1,j=10;i<10;++i ){
+	write(j)
+	++j 
+}
+if(1==1) 
+
+write('do');
+j = 1; 
+do{
+	write(j);
+	++j;
+}while(j<10);
+
+write('repeat');
+j = 1;
+repeat{
+	write(j);
+	++j;
+};
